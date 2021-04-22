c97b0832d7dde47ae8baf3e821f9faaeff5a97b6
robelcse <mdrobel.cse@gmail.com>
2021-04-23 04:04:58 +0600

    php multi-language website

---
+++ language.php
@@ -0,0 +1,35 @@
+<?php
+$top_nav=array(
+	'en'=>array('Home','About Us','Contact Us'),
+	'bn'=>array('হোম','আমাদের সম্পর্কে','যোগাযোগ করুন'),
+	'hi'=>array('होम','हमारे बारे मे','संपर्क करें')
+);
+
+$website_content=array(
+	'en'=>array('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas at suscipit urna. Mauris ac lectus aliquet, lobortis massa ac, eleifend risus. Donec sit amet libero et nisi venenatis laoreet nec at tortor. Sed dictum, est at varius convallis, justo mi bibendum nulla, eget finibus arcu ipsum in tortor. Nullam pulvinar varius egestas. Aliquam vel euismod quam, ut suscipit lacus. Suspendisse turpis enim, lobortis sit amet consectetur vitae, euismod et turpis.'),
+	'bn'=>array('আপনার পছন্দসই কাজটি করতে পারবেন না। সসিসিপিট অর্ন এ মেনেসেস। মাউরিস এসি লেক্টাস অ্যালিকিয়েট, লোবার্টিস ম্যাসা এসি, এলিফেন্ড রাইসেস। কৌতুকপূর্ণ সময় এবং নিখরচায় কাজ করা উচিত। তবে, এটি বিভিন্ন ধরণের উপর নির্ভর করে, ঠিক আমার মাইক্রোসফট, নির্মাতারা নির্যাতন শেষ। নুল্লাম পুলভিনার ভেরিয়াস এজেস্টাস। একইভাবে বা ইওজমোড থেকে সসেসিপিট। সর্বাধিক ঝাঁকুনি দেওয়া হয়, গিরিখাতী বাছাই করা হয়, ইওমসড এবং টারপিস।'),
+	'hi'=>array('लचकनहि क्षमता। जनित खरिदे रचना देखने सोफ़्टवेर विश्वव्यापि माहितीवानीज्य व्याख्या पुष्टिकर्ता माहितीवानीज्य समजते कलइस जानकारी प्राथमिक सकते संदेश बहुत कराना तकरीबन विभाजन तकनिकल दिनांक संपुर्ण जिसे पहोच। अत्यंत परस्पर संसाध जोवे नाकर सहायता विभाजनक्षमता बलवान विकास यधपि सारांश भारतीय सीमित करती जिम्मे व्यवहार प्राण सकता अंतर्गत पुर्णता दारी भारतीय हुएआदि समस्याए पुर्व सुचना जागरुक संस्थान विभाजनक्षमता पढाए बढाता ।क ऎसाजीस अन्तरराष्ट्रीयकरन')
+);
+
+
+
+
+
+// echo $top_nav['en'][0]."</br>";
+// echo $top_nav['en'][1]."</br>";
+// echo $top_nav['en'][2]."</br>";
+
+
+// echo $top_nav['bn'][0]."</br>";
+// echo $top_nav['bn'][1]."</br>";
+// echo $top_nav['bn'][2]."</br>";
+
+// echo $top_nav['hi'][0]."</br>";
+// echo $top_nav['hi'][1]."</br>";
+// echo $top_nav['hi'][2]."</br>";
+
+// echo $website_content['en'][0]."</br>";
+// echo $website_content['bn'][0]."</br>";
+// echo $website_content['hi'][0]."</br>";
+
+?>
